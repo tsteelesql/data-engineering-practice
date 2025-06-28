17e2b33bbdd9c1a8e48b1a9a2c36f891ff1708e3
Tyler <tsteelesql@gmail.com>
2025-06-28 11:59:23 -0400

    Exercise 1 - First verion; needs refactored

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
-import requests
+import requests, os, zipfile
+from urllib.parse import urlparse
+
+DIRECTORY_PATH = 'Exercises\Exercise-1\downloads'
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,10 +14,45 @@ download_uris = [
 ]
 
 
-def main():
-    # your code here
-    pass
+def delete_zip_file(zip_path):
+        os.remove(zip_path)
 
+
+def get_file_name(url):
+    parsed_url = urlparse(url)
+    return os.path.basename(parsed_url.path)
+
+
+def get_file(url,file_name):
+    # Send HTTP GET request
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Save the content to a local file
+        with open(f"{DIRECTORY_PATH}\\{file_name}", "wb") as file:
+            file.write(response.content)
+        print("Download completed successfully.")
+        unzip_file(file_name)
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+
+
+def unzip_file(file_name):
+    zip_path = os.path.join(DIRECTORY_PATH, file_name)
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(DIRECTORY_PATH)
+    print(f"Successfully unzipped file {file_name}")
+    delete_zip_file(zip_path)
+
+
+def main():
+    if not os.path.isdir(DIRECTORY_PATH):
+        os.mkdir(DIRECTORY_PATH)
+
+    for uri in download_uris:
+        file_name = get_file_name(uri)
+        get_file(uri, file_name)
 
 if __name__ == "__main__":
     main()

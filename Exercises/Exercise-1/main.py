e24f212514c4d67c3956822d9ae5806f7697e703
Tyler <tsteelesql@gmail.com>
2025-06-29 13:14:31 -0400

    Refactored code: 1) Swapped '\' to '/' in file path to remove syntax warning (could double \\ or raw string instead) 2) Changed directory + file to all use os.path.join, where not present for consistency 3) Updated try/except block for get_file() to have clearer error output 4) Added check that file existed before unzipping to unzip_file().

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,9 +1,9 @@
 import requests, os, zipfile
 from urllib.parse import urlparse
 
-DIRECTORY_PATH = 'Exercises\Exercise-1\downloads'
+DIRECTORY_PATH = "Exercises/Exercise-1/downloads"
 
-download_uris = [
+DOWNLOAD_URIS = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
@@ -25,32 +25,37 @@ def get_file_name(url):
 
 def get_file(url,file_name):
     # Send HTTP GET request
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        zip_file = os.path.join(DIRECTORY_PATH, file_name)
         # Save the content to a local file
-        with open(f"{DIRECTORY_PATH}\\{file_name}", "wb") as file:
+        with open(zip_file, "wb") as file:
             file.write(response.content)
         print("Download completed successfully.")
         unzip_file(file_name)
-    else:
-        print(f"Failed to download file. Status code: {response.status_code}")
+
+    except requests.RequestException as e:
+        print(f"Error downloading {file_name}:\n{e}")
+        return
 
 
 def unzip_file(file_name):
     zip_path = os.path.join(DIRECTORY_PATH, file_name)
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(DIRECTORY_PATH)
-    print(f"Successfully unzipped file {file_name}")
-    delete_zip_file(zip_path)
-
+    if zipfile.is_zipfile(zip_path):
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(DIRECTORY_PATH)
+        print(f"Successfully unzipped file {file_name}")
+        delete_zip_file(zip_path)
+    else:
+        print(f"{file_name} is not a valid zip file.")
+    
 
 def main():
     if not os.path.isdir(DIRECTORY_PATH):
         os.mkdir(DIRECTORY_PATH)
 
-    for uri in download_uris:
+    for uri in DOWNLOAD_URIS:
         file_name = get_file_name(uri)
         get_file(uri, file_name)
 
